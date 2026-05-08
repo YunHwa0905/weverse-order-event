3a1e16759ed251cc94c5d097bf54f02666443b8e
YunHwa0905 <yoho030905@gmail.com>
2026-05-08 15:38:13 +0900

    Git Init

---
+++ src/main/java/com/weverse/weverseorderevent/WeverseOrderEventApplication.java
@@ -0,0 +1,13 @@
+package com.weverse.weverseorderevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeverseOrderEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeverseOrderEventApplication.class, args);
+    }
+
+}
