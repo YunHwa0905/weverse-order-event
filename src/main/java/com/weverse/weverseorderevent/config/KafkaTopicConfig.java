f14686f046c79bdd9f74c2b533ce635c0ef9ad17
YunHwa0905 <yoho030905@gmail.com>
2026-05-14 10:32:11 +0900

    feat: kafka-config 설정

---
+++ src/main/java/com/weverse/weverseorderevent/config/KafkaTopicConfig.java
@@ -0,0 +1,78 @@
+package com.weverse.weverseorderevent.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.apache.kafka.clients.admin.NewTopic;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${kafka.topics.order}")
+    private String orderTopic;
+
+    @Value("${kafka.topics.order-dead-letter}")
+    private String orderDeadLetterTopic;
+
+    @Value("${kafka.topics.order-notification}")
+    private String orderNotificationTopic;
+
+    @Value("${kafka.topics.claim}")
+    private String claimTopic;
+
+    @Value("${kafka.topics.membership}")
+    private String membershipTopic;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        return new KafkaAdmin(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers));
+    }
+
+    @Bean
+    public NewTopic orderTopic() {
+        return TopicBuilder.name(orderTopic)
+                .partitions(2)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic orderDeadLetterTopic() {
+        return TopicBuilder.name(orderDeadLetterTopic)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic orderNotificationTopic() {
+        return TopicBuilder.name(orderNotificationTopic)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic claimTopic() {
+        return TopicBuilder.name(claimTopic)
+                .partitions(2)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic membershipTopic() {
+        return TopicBuilder.name(membershipTopic)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+}
